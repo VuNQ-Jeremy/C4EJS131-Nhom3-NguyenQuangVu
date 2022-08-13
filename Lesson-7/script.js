7379043d87aa776835aea53d02b9f728b4a2cd6d
VuNQ-Jeremy <110046050+VuNQ-Jeremy@users.noreply.github.com>
2022-08-13 20:26:15 +0700

    nothing

---
+++ Lesson-7/script.js
@@ -0,0 +1,10 @@
+// alert('Chao cac ban');
+// console.log('Chao cac ban');
+// confirm('Bam vao nut OK de dong y va tiep tuc');
+// prompt('Nhap ho ten cua ban')
+
+let username ='Vũ';
+let age = 25;
+let email ='ngqv0712@gmail.com';
+
+console.log('username:', username);
