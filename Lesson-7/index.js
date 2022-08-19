ec002f8c212bc498d2a0d9e8ca2a235448d3b70e
VuNQ-Jeremy <110046050+VuNQ-Jeremy@users.noreply.github.com>
2022-08-20 06:09:45 +0700

    fix

--- Lesson-7/index.js
+++ Lesson-7/index.js
@@ -1,12 +1,80 @@
+// var fullname = "Tam";
+
+// var fullname;
+// fullname = "Tam2";
+
+// let fullname = "Tam";
+// // let fullname = "Tam";
+// console.log("fullname", fullname);
+
+// fullname = "Nam";
+// console.log("fullname", fullname);
+
+// const fullname = "Tam";
+
+// console.log("fullname", fullname);
+
+// console.log(fullname);
+
+// var fullname = 1;
+
+// const fullname = "Tam";
+
+// const doSth = () => {
+//   const fullname = "Nam";
+//   console.log(fullname);
+// };
+
+// doSth();
+
 // let str = "Demo lop CI131";
-// str1 = `Hom nay ${str}`;
+// console.log(str.slice(5, 9));
 
-// console.log(str1);
+// str = "Hom nay" + "\n" + str;
 
-// str2 = `Hom nay\n${str}`;
+// str = "Hom nay" + " " + str;
+// str = `Hom nay
+// ${str}`;
 
-// console.log(str2);
+// console.log(str);
 
-let arr =[1, 2, 3, 4, [5, 6, 7, [8, 9, 10]]];
+// let str = "Along";
 
-console.log(arr[4][3][1]);
+// const character = str.charAt(6);
+// const character = str[6];
+// console.log(str.toUpperCase());
+
+// let arr = [1, 2, 3];
+
+// arr.push(4);
+// const a = arr.pop();
+// arr.pop();
+
+// console.log(arr);
+
+const obj = {
+    key1: "1",
+    key2: "2",
+  };
+  
+  console.log(obj["key1"]);
+  console.log(obj.key1);
+  
+  // const arr = [1, 2, 3, 4];
+  
+  // // console.log(arr[0]);
+  
+  // console.log(arr?.[0]);
+  
+  // const username = "A";
+  // const userAge = 18;
+  
+  // const user = {
+  //   name: "A",
+  //   age: 18,
+  // };
+  
+  // const userArr = [
+  //   { name: "A", age: 18, location: "" },
+  //   { name: "B", age: 20, location: "" },
+  // ];
