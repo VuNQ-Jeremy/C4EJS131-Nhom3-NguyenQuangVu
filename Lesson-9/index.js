d93b344f550ebda9d3783e7bb570bf6040e5466a
VuNQ-Jeremy <110046050+VuNQ-Jeremy@users.noreply.github.com>
2022-08-30 17:44:30 +0700

    note

---
+++ Lesson-9/index.js
@@ -0,0 +1,30 @@
+// Function declaration
+
+function abc(x,y,z) {
+    //code
+}
+
+abc('d','e','f');
+abc('g','h','i');
+
+let abcd1 = function () {
+    //code
+}
+
+// Arrow function
+
+let abcd2 = (name, age, favorites) => console.log('are you ok');
+
+abcd2('name', 'age', 'favorites');
+
+
+//Function return
+
+function sub(a,b) {
+    //trừ a cho b
+    return a-b;
+}
+
+let result =  sub(10,3);
+console.log(result);
+
